0c8b1234b88480cc676677e8f904cef87911c19f
Haris Razis <haris@razis.com>
2022-05-11 23:00:41 +0300

    Add landing screen ps implementation

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,23 +1,13 @@
-if (document.getElementById('lightbox-landing')) {
-    lightGallery(document.getElementById('lightbox-landing'), {
-        thumbnail: true,
-        download: false,
-        selector: '.lb',
-        plugins: [lgThumbnail],
+import PhotoSwipeLightbox from '../libs/PhotoSwipe/dist/photoswipe-lightbox.esm.js';
+
+if (document.getElementsByClassName('lightbox')) {
+    let lightbox
+
+    lightbox = new PhotoSwipeLightbox({
+        gallery: '.lightbox',
+        children: '.lb',
+        pswpModule: () => import('../libs/PhotoSwipe/dist/photoswipe.esm.js')
     });
-}
 
-if (document.getElementsByClassName('lightbox-room')) {
-    const lightboxes = document.querySelectorAll('.lightbox-room').length;
-
-    for (let index = 0; index < lightboxes; index++) {
-        let lightbox = document.getElementById(`lightbox-room-${index}`);
-
-        lightGallery(lightbox, {
-            thumbnail: true,
-            download: false,
-            selector: '.lb',
-            plugins: [lgThumbnail],
-        });
-    }
+    lightbox.init();
 }
